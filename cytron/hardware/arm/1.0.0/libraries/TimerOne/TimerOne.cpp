b11bf567029469e4cbc89b73b1927610dbdc01bc
bengchet <bengchet@cytron.com.my>
2016-01-07 16:02:30 +0800

    added TimerOne library

---
+++ cytron/hardware/arm/1.0.0/libraries/TimerOne/TimerOne.cpp
@@ -0,0 +1,21 @@
+/*
+ *  Interrupt and PWM utilities for 16 bit Timer1 on ATmega168/328
+ *  Original code by Jesse Tane for http://labs.ideo.com August 2008
+ *  Modified March 2009 by Jérôme Despatis and Jesse Tane for ATmega328 support
+ *  Modified June 2009 by Michael Polli and Jesse Tane to fix a bug in setPeriod() which caused the timer to stop
+ *  Modified Oct 2009 by Dan Clemens to work with timer1 of the ATMega1280 or Arduino Mega
+ *  Modified April 2012 by Paul Stoffregen
+ *  Modified again, June 2014 by Paul Stoffregen
+ *
+ *  This is free software. You can redistribute it and/or modify it under
+ *  the terms of Creative Commons Attribution 3.0 United States License. 
+ *  To view a copy of this license, visit http://creativecommons.org/licenses/by/3.0/us/ 
+ *  or send a letter to Creative Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
+ *
+ */
+
+#include "TimerOne.h"
+
+#if(NR_TIMERS>0)
+	HardwareTimer Timer1(0,TMR0_MODULE,CLK_CLKSEL1_TMR0_S_HXT);
+#endif
