6ae92980422d4a1c9e3a16ecc52f21bb4b374c6e
bengchet <bengchet@cytron.com.my>
2016-01-15 11:47:00 +0800

    updated servo library

--- cytron/hardware/arm/1.0.0/libraries/Servo/src/Servo.cpp
+++ cytron/hardware/arm/1.0.0/libraries/Servo/src/Servo.cpp
@@ -18,34 +18,70 @@
 #include "Arduino.h"
 #include "Servo.h"
 
-int Servo_MAX=100;
-int Servo_MIN=0;
-int Servo_Pin=0;
-int Servo_Val=0;
+Servo::Servo():
+	Servo_MAX(12),
+	Servo_MIN(3),
+	Servo_Freq(50),
+	Servo_Pin(0),
+	Servo_Val(0),
+	bpwm(0)
+{}
 
-Servo::Servo()
-{
+//static uint8_t pinEnabled[PWM_MAX_COUNT];
+//static uint32_t fixValue[PWM_MAX_COUNT];
 
-}
-
-uint8_t Servo::attach(int pin)
-{
-  return this->attach(pin, 0, 100);
-}
-
-static uint8_t pinEnabled[PWM_MAX_COUNT];
-static uint32_t fixValue[PWM_MAX_COUNT];
-
-uint8_t Servo::attach(int ulPin, int min, int max)
+uint8_t Servo::attach(int ulPin, int min, int max, int freq)
 {      
 	uint32_t ulValue=0;
+	bpwm = 0;
+	
 	#ifdef USE_BoardToPin
 	if(ulPin > BoardToPin_MAX_COUNT) return 0;
+	
+	if(ulPin > 5 && ulPin < 10) { // BPWM pins - 6, 7, 8, 9
+		bpwm = 1;
+	}
+	
+	if(ulPin > 17 && ulPin < 22) { // PWM at analog pins - A0, A1, A2, A3
+		ulPin = ulPin + 8;
+	}
+	
 	if(BoardToPinInfo[ulPin].type!=PWM_TYPE) return 0;
+	
 	ulPin=BoardToPinInfo[ulPin].num;
+	
+	if(bpwm) {
+		if(ulPin > BPWM_MAX_COUNT || BPWM_Desc[ulPin].P == NULL) return 0;
+	}
+	else {
+		if(ulPin  > PWM_MAX_COUNT || PWM_Desc[ulPin].P == NULL) return 0;
+	}
 	#else
-	if(ulPin>PWM_MAX_COUNT || PWM_Desc[ulPin].P==NULL) return 0;
+		return 0;
 	#endif	
+	
+	if(bpwm) 
+	{
+		BPWM_Config(BPWM_Desc[ulPin]); // Set Mutifunction pins
+		BPWM_ConfigOutputChannel(BPWM_Desc[ulPin].P, BPWM_Desc[ulPin].ch, BPWM_Desc[ulPin].freq, ulValue); // Config BPWMs
+		BPWM_EnableOutput(BPWM_Desc[ulPin].P, (1 << BPWM_Desc[ulPin].ch)); //Enable BPWM output
+		BPWM_Start(BPWM_Desc[ulPin].P, (1 << BPWM_Desc[ulPin].ch)); //Start BPWM
+	}
+	
+	else 
+	{
+		PWM_Config(PWM_Desc[ulPin]); // Set Mutifunction pins
+		PWM_ConfigOutputChannel(PWM_Desc[ulPin].P, PWM_Desc[ulPin].ch, PWM_Desc[ulPin].freq, ulValue); // Config PWMs
+		PWM_EnableOutput(PWM_Desc[ulPin].P, (1 << PWM_Desc[ulPin].ch)); //Enable PWM output
+		PWM_Start(PWM_Desc[ulPin].P, (1 << PWM_Desc[ulPin].ch)); //Start PWM
+	}
+	
+	Servo_MAX = max;
+	Servo_MIN = min;
+	Servo_Pin = ulPin;
+	Servo_Freq = freq;
+				
+/*
 #if defined(__M451__)
 	if(ulValue==100)
 	{  
@@ -80,9 +116,10 @@ uint8_t Servo::attach(int ulPin, int min, int max)
 		//return 0;
 	}
 #elif defined(__NUC131__)
+
 	if(ulValue==0)
 	{  
-		int32_t pin=PWM_Desc[ulPin].pintype.num;
+		uint32_t pin = PWM_Desc[ulPin].pintype.num;
 		GPIO_Config(GPIO_Desc[pin]);
 		GPIO_SetMode(GPIO_Desc[pin].P, GPIO_Desc[pin].bit, GPIO_PMD_OUTPUT);
 		(GPIO_Desc[pin].P)->DOUT &= ~GPIO_Desc[pin].bit;
@@ -90,10 +127,11 @@ uint8_t Servo::attach(int ulPin, int min, int max)
 		fixValue[ulPin]=ulValue;
 		//return 0;
 	}
-#endif	
+#endif
 
 	if (!pinEnabled[ulPin]){
 		//Set Mutifunction pins
+
 		PWM_Config(PWM_Desc[ulPin]);		
     
 		//Config PWMs
@@ -114,14 +152,17 @@ uint8_t Servo::attach(int ulPin, int min, int max)
 		PWM_ConfigOutputChannel(PWM_Desc[ulPin].P,PWM_Desc[ulPin].ch,50,ulValue);
 		fixValue[ulPin]=ulValue;
 	}
-	
-  Servo_MAX=max;
-  Servo_MIN=min;
-  Servo_Pin=ulPin;
+*/
+
 }
 
 void Servo::detach()
 {
+	if(bpwm)
+		BPWM_DisableOutput(BPWM_Desc[Servo_Pin].P,(1<<BPWM_Desc[Servo_Pin].ch));
+	
+	else
+		PWM_DisableOutput(PWM_Desc[Servo_Pin].P,(1<<PWM_Desc[Servo_Pin].ch));	
 
 }
 
@@ -131,11 +172,15 @@ void Servo::write(int value)
       value = 0;
     else if (value > 180)
       value = 180;
+  	
+	Servo_Val = value;
 
-    //value = map(value, 0, 180, Servo_MIN, Servo_MAX);
-    value = map(value, 0, 180, 5, 12);
-    Servo_Val=value;
-	  PWM_ConfigOutputChannel(PWM_Desc[Servo_Pin].P,PWM_Desc[Servo_Pin].ch,50,Servo_Val);	 
+    value = map(value, 0, 180, Servo_MIN, Servo_MAX);
+    //value = map(value, 0, 180, 5, 12);
+	if(bpwm)
+		BPWM_ConfigOutputChannel(BPWM_Desc[Servo_Pin].P, BPWM_Desc[Servo_Pin].ch, Servo_Freq, value);
+	else
+		PWM_ConfigOutputChannel(PWM_Desc[Servo_Pin].P,PWM_Desc[Servo_Pin].ch,Servo_Freq,value);	 
 }
 
 int Servo::read() // return the value as degrees
--- cytron/hardware/arm/1.0.0/libraries/Servo/src/Servo.h
+++ cytron/hardware/arm/1.0.0/libraries/Servo/src/Servo.h
@@ -55,19 +55,24 @@
 class Servo
 {
 public:
+
   Servo();
-  uint8_t attach(int pin);           // attach the given pin to the next free channel, sets pinMode, returns channel number or 0 if failure
-  uint8_t attach(int pin, int min, int max); // as above but also sets min and max values for writes. 
+  //uint8_t attach(int pin);           // attach the given pin to the next free channel, sets pinMode, returns channel number or 0 if failure
+  uint8_t attach(int pin, int min = 3, int max = 12, int freq = 50); // as above but also sets min and max values for writes. 
   void detach();
   void write(int value);             // if value is < 200 its treated as an angle, otherwise as pulse width in microseconds 
-//  void writeMicroseconds(int value); // Write pulse width in microseconds 
+  // void writeMicroseconds(int value); // Write pulse width in microseconds 
   int read();                        // returns current pulse width as an angle between 0 and 180 degrees
- // int readMicroseconds();            // returns current pulse width in microseconds for this servo (was read_us() in first release)
+  // int readMicroseconds();            // returns current pulse width in microseconds for this servo (was read_us() in first release)
   bool attached();                   // return true if this servo is attached, otherwise false 
 private:
-   uint8_t servoIndex;               // index into the channel data for this servo
-   int8_t min;                       // minimum is this value times 4 added to MIN_PULSE_WIDTH    
-   int8_t max;                       // maximum is this value times 4 added to MAX_PULSE_WIDTH   
+   uint8_t bpwm;
+   int Servo_MAX;
+   int Servo_MIN;
+   int Servo_Freq;
+   int Servo_Pin;
+   int Servo_Val;
+   
 };
 
 #endif
