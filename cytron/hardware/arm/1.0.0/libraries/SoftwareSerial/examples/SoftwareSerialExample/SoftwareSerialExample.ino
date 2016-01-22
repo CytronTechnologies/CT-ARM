02b706c46815e3463b2b2ec1a8b1048e9f68c370
bengchet <bengchet@cytron.com.my>
2016-01-22 12:18:15 +0800

    updated SoftwareSerial example sketch

--- cytron/hardware/arm/1.0.0/libraries/SoftwareSerial/examples/SoftwareSerialExample/SoftwareSerialExample.ino
+++ cytron/hardware/arm/1.0.0/libraries/SoftwareSerial/examples/SoftwareSerialExample/SoftwareSerialExample.ino
@@ -31,7 +31,7 @@ SoftwareSerial mySerial(2, 3); // RX, TX
 
 void setup() {
   // Open serial communications and wait for port to open:
-  Serial.begin(57600);
+  Serial.begin(115200);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
@@ -40,14 +40,16 @@ void setup() {
   Serial.println("Goodnight moon!");
 
   // set the data rate for the SoftwareSerial port
-  mySerial.begin(4800);
+  mySerial.begin(115200);
   mySerial.println("Hello, world?");
-  mySerial.setTimeout(500);
 }
 
 void loop() { // run over and over
   if (mySerial.available()) {
-    mySerial.println(mySerial.readString());
+    Serial.write(mySerial.read());
+  }
+  if (Serial.available()) {
+    mySerial.write(Serial.read());
   }
 }
 
