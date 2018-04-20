a0944c22305fcedd358b2506f9c23182900092a0
Ng Beng Chet <bengchet@cytron.com.my>
2018-04-20 23:08:14 +0800

    Update build_boards_manager_package.sh

--- package/build_boards_manager_package.sh
+++ package/build_boards_manager_package.sh
@@ -96,11 +96,11 @@ jq ".packages[0].platforms[0].version = \"$ver\" | \
 echo "Getting nuvodude stable version"
 curl --silent https://api.github.com/repos/$ORGANISATION/nuvodude/releases > releases.json
 # Previous final release (prerelase == false)
-prev_release=$(jq -r '. | map(select(.draft == false and .prerelease == false)) | sort_by(.created_at | - fromdateiso8601) | .[0].tag_name' releases.json)
+prev_release=`jq -r '. | map(select(.draft == false and .prerelease == false)) | sort_by(.created_at | - fromdateiso8601) | .[0].tag_name' releases.json`
 # Previous release (possibly a pre-release)
-prev_any_release=$(jq -r '. | map(select(.draft == false)) | sort_by(.created_at | - fromdateiso8601)  | .[0].tag_name' releases.json)
+prev_any_release=`jq -r '. | map(select(.draft == false)) | sort_by(.created_at | - fromdateiso8601)  | .[0].tag_name' releases.json`
 # Previous pre-release
-prev_pre_release=$(jq -r '. | map(select(.draft == false and .prerelease == true)) | sort_by(.created_at | - fromdateiso8601)  | .[0].tag_name' releases.json)
+prev_pre_release=`jq -r '. | map(select(.draft == false and .prerelease == true)) | sort_by(.created_at | - fromdateiso8601)  | .[0].tag_name' releases.json`
 
 echo "Previous release: $prev_release"
 echo "Previous (pre-?)release: $prev_any_release"
